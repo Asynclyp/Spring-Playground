df1d6378c0a4355f8314f85157720b65778bfedd
Yuanpeng Li <l.yuanpeng@wustl.edu>
2024-06-16 16:01:14 +0800

    move config class into main class

--- learn-spring-framework-2/src/main/java/com/yuanpeng/learn_spring_framework/App03GamingSpring.java
+++ learn-spring-framework-2/src/main/java/com/yuanpeng/learn_spring_framework/App03GamingSpring.java
@@ -1,7 +1,35 @@
 package com.yuanpeng.learn_spring_framework;
 
 import com.yuanpeng.learn_spring_framework.game.*;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class GameConfiguration {
+    @Bean
+    @Qualifier("mario")
+    public GameConsole mario(){
+        return new MarioGame();
+    }
+
+    @Bean
+    public GameConsole superContra(){
+        return new SuperContraGame();
+    }
+
+    @Bean
+    public GameConsole pacman(){
+        return new PacmanGame();
+    }
+
+    @Bean
+    public GameRunner marioRunner(@Qualifier("mario")GameConsole mario){
+        return new GameRunner(mario);
+    }
+
+}
 
 public class App03GamingSpring {
     public static void main(String[] args) {
