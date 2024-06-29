6aed91a6402e65d24c7b8b4e45b1e7dbb9442fe3
Yuanpeng Li <l.yuanpeng@wustl.edu>
2024-06-29 12:04:12 -0500

    finish scope

---
+++ learn-spring-framework-2/src/main/java/examples/d1/a0/LazyInitializationLauncherApplication.java
@@ -0,0 +1,39 @@
+package examples.d1.a0;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+@Component
+class ClassA{
+
+}
+@Component
+@Lazy
+class ClassB{
+    private ClassA classA;
+
+    public ClassB(ClassA classA) {
+        System.out.println("Initialization logic for class B");
+        this.classA = classA;
+    }
+
+    public void doSomething() {
+        System.out.println("Some do something");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class LazyInitializationLauncherApplication {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApplication.class);
+//        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        System.out.println("Initialization competed");
+        context.getBean(ClassB.class).doSomething();
+    }
+}
